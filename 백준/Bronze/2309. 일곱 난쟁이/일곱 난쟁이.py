09a4dc75e9e0ff90bd55cae4a29da4816f00686b
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2023-12-28 02:12:16 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,29 +1,20 @@
 nineShorts = []
 sevenShorts = []
-sumH = 0
-exceptNum1, exceptNum2 = 0, 1
 
 for i in range(9):
     nineShorts.append(int(input()))
 
-while True:
-    sumH = sum(nineShorts) - (nineShorts[exceptNum1] + nineShorts[exceptNum2])
-    if sumH == 100:
-        for i in range(9):
-            if i == exceptNum1:
-                continue
-            elif i == exceptNum2:
-                continue
-            else:
-                sevenShorts.append(nineShorts[i])
-        break
+val = sum(nineShorts) - 100
 
-    if exceptNum2 == 8:
-        exceptNum1 += 1
-        exceptNum2 = exceptNum1 + 1
-    else:
-        exceptNum2 += 1
+for a in range(9):
+    for b in range(a + 1, 9):
+        if nineShorts[a] + nineShorts[b] == val:
+            x = nineShorts[a]
+            y = nineShorts[b]
 
-sevenShorts.sort()
-for i in range(7):
-    print(sevenShorts[i])
+nineShorts.remove(x)
+nineShorts.remove(y)
+nineShorts.sort()
+
+for i in nineShorts:
+    print(i)
