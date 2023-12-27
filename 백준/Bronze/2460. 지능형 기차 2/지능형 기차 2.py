5c547276892e8962dea6cfafe4c9db9339ea98e1
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2023-12-28 01:04:10 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.py
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.py
@@ -2,11 +2,9 @@ STOP = 10
 
 outPerson, inPerson = 0, 0
 maxPerson, currentPerson = 0, 0
-
 for i in range(STOP):
     outPerson, inPerson = map(int, input().split())
     currentPerson += (inPerson - outPerson)
-    if currentPerson > maxPerson:
-        maxPerson = currentPerson
-    
+    maxPerson = max(maxPerson, currentPerson)
+
 print(maxPerson)
