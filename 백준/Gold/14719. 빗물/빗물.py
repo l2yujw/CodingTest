2acb04bda9a531db3d5f9084779d739acb6aef21
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2024-02-07 17:39:30 +0900

    [Gold V] Title: 빗물, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -4,31 +4,15 @@ input = stdin.readline
 
 H, W= map(int, input().split())
 num = list(map(int, input().split()))
+ans = 0
 
-frame = [[0]*W for _ in range(H)]
-posX = 0
-for i in num:
-    posY = H - 1
-    for _ in range(i):
-        frame[posY][posX] = 1
-        posY -= 1
-    posX += 1
+for i in range(1, W-1):#1~3 1 2
+    left_max = max(num[:i])
+    right_max = max(num[i+1:])
 
-total = 0
-for i in range(H):
-    wall = 0
-    water = 0
-    for x in range(W):
-        if frame[i][x] == 1:
-            wall += 1
-            if wall == 2:
-                total += water
-                water = 0
-                wall -= 1
-        elif frame[i][x] == 0:
-            if wall == 0:
-                continue
-            else:
-                water += 1
+    compare = min(left_max, right_max)
 
-print(total)
+    if num[i] < compare:
+        ans += compare - num[i]
+
+print(ans)
