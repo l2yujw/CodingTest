010667eb243964aa1ac6fd947b6b8c54f7b615e4
ryu <jeungwon28@gmail.com>
2024-02-08 22:09:06 +0900

    더 간략하게 생각할것

--- 백준/문제풀이노트2.py
+++ 백준/문제풀이노트2.py
@@ -1,27 +1,18 @@
-from itertools import combinations
-n, k = map(int, input().split())
-if k < 5:
-    print(0)
-else:
-    k -= 5
-    nece_chars = {'a', 'n', 't', 'i', 'c'}
-    input_chars = []
-    alpha = {ky: v for v, ky in enumerate(
-        (set(map(chr, range(ord('a'), ord('z')+1))) - nece_chars))}
-    cnt = 0
-    for _ in range(n):
-        tmp = 0
-        for c in set(input())-nece_chars:
-            tmp |= (1 << alpha[c])
-        input_chars.append(tmp)
-    power_by_2 = (2**i for i in range(21))
-    for comb in combinations(power_by_2, k):
-        test = sum(comb)
+N, S = map(int, input().split())
+arr = list(map(int, input().split()))
 
-        ct = 0
-        for cb in input_chars:
-            if test & cb == cb:
-                ct += 1
+start, end = 0, 0
+sum_ = arr[0]
+ans = 100001
 
-        cnt = max(cnt, ct)
-    print(cnt)
+while True:
+    if sum_ < S:
+        end += 1
+        if end == N: break
+        sum_ += arr[end]
+    else:
+        sum_ -= arr[start]
+        ans = min(ans, end - start + 1)
+        start += 1
+
+print(ans if ans != 100001 else 0)
