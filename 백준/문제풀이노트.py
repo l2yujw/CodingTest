0f247490402aa88c4dc7d192a076b211aa076a00
ryu <jeungwon28@gmail.com>
2024-02-11 22:09:09 +0900

    사탕게임 dx dy 활용

--- 백준/문제풀이노트.py
+++ 백준/문제풀이노트.py
@@ -1,41 +1,63 @@
+# NXN 사탕
+# 사탕색 다름
+# 사탕의 색이 다른 인접한 두칸을 선택
+# 두 사탕을 교환
+# 모두 같은 색으로 이루어져 잇는 가장 긴 연속 부분을 고르고 삭제
+# 이때의 최대 개수
 from sys import stdin
 
 input = stdin.readline
-N ,S= map(int, input().split())
-seq = list(map(int, input().split()))
 
-minLen = 0
-partSum = 0
-tmpLen = 0
-solvLen = 0
-#최소길이 or 0
+N = int(input())
 
-def solv():
-    global partSum
-    global tmpLen
-    global minLen
-    global solvLen
+# C P Z Y
+arr = [list(input()) for _ in range(N)]
+maxCount = 0
+
+def solve():
+    global arr
+    for x in range(N):
+        for kCol in range(N-1):
+            if arr[x][kCol] == arr[x][kCol+1]:
+                continue
+            arr[x][kCol], arr[x][kCol+1] = arr[x][kCol+1], arr[x][kCol]
+            findMax(arr)
+            arr[x][kCol], arr[x][kCol + 1] = arr[x][kCol + 1], arr[x][kCol]
+
+        for kRow in range(N - 1):
+            if arr[kRow][x] == arr[kRow + 1][x]:
+                continue
+
+            arr[kRow][x], arr[kRow + 1][x] = arr[kRow + 1][x], arr[kRow][x]
+            findMax(arr)
+            arr[kRow][x], arr[kRow + 1][x] = arr[kRow + 1][x], arr[kRow][x]
+
+def findMax(arr):
+    global maxCount
+    global N
 
     for x in range(N):
-        start = x
-        partSum = 0
-        tmpLen = 0
-        while partSum < S:
-            partSum += seq[start]
-            tmpLen += 1
+        count = 0
+        for kRow in range(N - 1):
+            cur = arr[x][kRow]
+            count += 1
+            if cur != arr[x][kRow+1]:
+                maxCount = max(maxCount, count)
+                count = 0
+            elif kRow == N-2:
+                count += 1
+                maxCount = max(maxCount, count)
 
-            if start == N-1 and partSum < S:
-                tmpLen = minLen
-                break
-            elif start == N-1:
-                break
-            else:
-                start += 1
+        count = 0
+        for kCol in range(N - 1):
+            cur = arr[kCol][x]
+            count += 1
+            if cur != arr[kCol + 1][x]:
+                maxCount = max(maxCount, count)
+                count = 0
+            elif kCol == N-2:
+                count += 1
+                maxCount = max(maxCount, count)
 
-        if solvLen == 0:
-            minLen = tmpLen
-        solvLen += 1
-        minLen = min(minLen, tmpLen)
-
-solv()
-print(minLen)
+solve()
+print(maxCount)
