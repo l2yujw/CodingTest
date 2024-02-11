ec0c304bc03a186287dce639b6c149bf8b323e1f
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2024-02-11 22:09:48 +0900

    [Silver II] Title: 사탕 게임, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -1,57 +1,39 @@
-from sys import stdin
+import sys
+input = sys.stdin.readline
 
-input = stdin.readline
+answer = 0
+dx = [-1, 1, 0, 0]
+dy = [0, 0, 1, -1]
 
-N = int(input())
 
-# C P Z Y
-arr = [list(input()) for _ in range(N)]
-maxCount = 0
+def max_eat(x, y):
+    cnt_x, cnt_y = 1, 1
+    for k in range(4):
+        xx, yy = x+dx[k], y+dy[k]
+        while 0 <= xx < N and 0 <= yy < N and board[x][y] == board[xx][yy]:
+            if k < 2:
+                cnt_x += 1
+            else:
+                cnt_y += 1
+            xx += dx[k]
+            yy += dy[k]
+    return max(cnt_x, cnt_y)
 
-def solve():
-    global arr
-    for x in range(N):
-        for kCol in range(N-1):
-            if arr[x][kCol] == arr[x][kCol+1]:
-                continue
-            arr[x][kCol], arr[x][kCol+1] = arr[x][kCol+1], arr[x][kCol]
-            findMax(arr)
-            arr[x][kCol], arr[x][kCol + 1] = arr[x][kCol + 1], arr[x][kCol]
 
-        for kRow in range(N - 1):
-            if arr[kRow][x] == arr[kRow + 1][x]:
-                continue
-
-            arr[kRow][x], arr[kRow + 1][x] = arr[kRow + 1][x], arr[kRow][x]
-            findMax(arr)
-            arr[kRow][x], arr[kRow + 1][x] = arr[kRow + 1][x], arr[kRow][x]
-
-def findMax(arr):
-    global maxCount
-    global N
-
-    for x in range(N):
-        count = 0
-        for kRow in range(N - 1):
-            cur = arr[x][kRow]
-            count += 1
-            if cur != arr[x][kRow+1]:
-                maxCount = max(maxCount, count)
-                count = 0
-            elif kRow == N-2:
-                count += 1
-                maxCount = max(maxCount, count)
-
-        count = 0
-        for kCol in range(N - 1):
-            cur = arr[kCol][x]
-            count += 1
-            if cur != arr[kCol + 1][x]:
-                maxCount = max(maxCount, count)
-                count = 0
-            elif kCol == N-2:
-                count += 1
-                maxCount = max(maxCount, count)
-
-solve()
-print(maxCount)
+if __name__ == "__main__":
+    N = int(input())
+    board = [list(map(str, input().rstrip())) for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            answer = max(answer, max_eat(i, j))
+            if j+1 < N and board[i][j] != board[i][j+1]:
+                board[i][j], board[i][j+1] = board[i][j+1], board[i][j]
+                answer = max(answer, max_eat(i, j))
+                answer = max(answer, max_eat(i, j+1))
+                board[i][j], board[i][j+1] = board[i][j+1], board[i][j]
+            if i+1 < N and board[i][j] != board[i+1][j]:
+                board[i][j], board[i+1][j] = board[i+1][j], board[i][j]
+                answer = max(answer, max_eat(i, j))
+                answer = max(answer, max_eat(i+1, j))
+                board[i][j], board[i+1][j] = board[i+1][j], board[i][j]
+    print(answer)
