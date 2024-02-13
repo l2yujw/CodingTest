68bee625a2e995acf7c99f0438660a6a03b5a1c2
ryu <jeungwon28@gmail.com>
2024-02-14 00:46:34 +0900

    find

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기_find.py
@@ -0,0 +1,5 @@
+symbol = 'abcdefghijklmnopqrstuvwxyz'
+a = input()
+for i in symbol:
+	ind = a.find(i)
+	print(ind, end=' ')
--- 백준/문제풀이노트.py
+++ 백준/문제풀이노트.py
@@ -1,13 +1,8 @@
-N = int(input())
-OX = list(input().rstrip() for _ in range(N))
+S = input()
+abc ='abcdefghijklmnopqrstuvwxyz'
 
-for y in range(N):
-    count, res = 0, 0
-    for x in OX[y]:
-        if x == 'O':
-            count += 1
-        else:
-            res += count * (count + 1) // 2
-            count = 0
-    res += count * (count + 1) // 2
-    print(res)
+for i in abc:
+    if i in S:
+        print(S.index(i), end= ' ')
+    else:
+        print( -1, end =' ')
