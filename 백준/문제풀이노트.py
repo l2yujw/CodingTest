f753ad4a7c2aeea4b0a9e4045bee3f6a6b155653
ryu <jeungwon28@gmail.com>
2024-02-16 02:04:43 +0900

    ADD: 스택수열_2

--- 백준/문제풀이노트.py
+++ 백준/문제풀이노트.py
@@ -1,10 +1,22 @@
-while True:
-    answer = 'yes'
-    num = list(map(str, input()))
-    if num[0] == '0':
-        break
-    for x in range(len(num) // 2):
-        if num[x] != num[-x - 1]:
-            answer = 'no'
-            break
-    print(answer)
+from sys import stdin
+
+input = stdin.readline
+n = int(input())
+soo = []
+for _ in range(n):
+    soo.append(int(input()))
+
+def numeric():
+    cnt, stack, result = 0, [], []
+    for i in soo:
+        while cnt < i:
+            cnt += 1
+            stack.append(cnt)
+            result.append('+')
+        if stack.pop() != i:
+            return 'NO'
+        else:
+            result.append('-')
+    return '\n'.join(result)
+
+print(numeric())
