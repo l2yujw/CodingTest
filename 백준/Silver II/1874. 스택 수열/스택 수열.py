8faa1ff4e955d828d4eb0c9262afa7f20b46ac65
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2024-02-16 02:02:29 +0900

    [Silver II] Title: 스택 수열, Time: 96 ms, Memory: 36952 KB -BaekjoonHub

--- 백준/Silver II/1874. 스택 수열/스택 수열.py
+++ 백준/Silver II/1874. 스택 수열/스택 수열.py
@@ -1,28 +1,22 @@
-n = int(input())
+from sys import stdin
 
+input = stdin.readline
+n = int(input())
 soo = []
-pos = 0
 for _ in range(n):
     soo.append(int(input()))
 
-arr = []
-answer = []
-for x in range(1, n + 1):
-    arr.append(x)
-    answer.append('+')
-    while True:
-        if len(arr) != 0:
-            tmp = arr.pop()
+def numeric():
+    cnt, stack, result = 0, [], []
+    for i in soo:
+        while cnt < i:
+            cnt += 1
+            stack.append(cnt)
+            result.append('+')
+        if stack.pop() != i:
+            return 'NO'
         else:
-            break
-        if tmp == soo[pos]:
-            answer.append('-')
-            pos += 1
-        else:
-            arr.append(tmp)
-            break
+            result.append('-')
+    return '\n'.join(result)
 
-if len(arr) == 0:
-    print("\n".join(answer))
-else:
-    print("NO")
+print(numeric())
