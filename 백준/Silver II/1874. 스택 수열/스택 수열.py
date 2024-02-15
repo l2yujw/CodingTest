18c9398f238290244ee2acd92005b20ff0ec622a
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2024-02-16 01:35:01 +0900

    [Silver II] Title: 스택 수열, Time: 4020 ms, Memory: 37344 KB -BaekjoonHub

---
+++ 백준/Silver II/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,28 @@
+n = int(input())
+
+soo = []
+pos = 0
+for _ in range(n):
+    soo.append(int(input()))
+
+arr = []
+answer = []
+for x in range(1, n + 1):
+    arr.append(x)
+    answer.append('+')
+    while True:
+        if len(arr) != 0:
+            tmp = arr.pop()
+        else:
+            break
+        if tmp == soo[pos]:
+            answer.append('-')
+            pos += 1
+        else:
+            arr.append(tmp)
+            break
+
+if len(arr) == 0:
+    print("\n".join(answer))
+else:
+    print("NO")
