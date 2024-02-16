cbafdf1fdc497bf35558906dd43d4beee9db21b6
ryu <jeungwon28@gmail.com>
2024-02-16 13:06:35 +0900

    수찾기_함수 활용

--- 백준/문제풀이노트.py
+++ 백준/문제풀이노트.py
@@ -1,22 +1,26 @@
-from sys import stdin
+from collections import deque
 
-input = stdin.readline
-n = int(input())
-soo = []
-for _ in range(n):
-    soo.append(int(input()))
+num = int(input())
+for i in range(num):
+    cnt = 0
+    N, M = map(int, input().split())
+    imp = deque(list(map(int, input().split())))
 
-def numeric():
-    cnt, stack, result = 0, [], []
-    for i in soo:
-        while cnt < i:
-            cnt += 1
-            stack.append(cnt)
-            result.append('+')
-        if stack.pop() != i:
-            return 'NO'
+    while len(imp) > 0:
+        maxi = max(imp)
+        if imp[0] == maxi:
+            imp.popleft()
+            if M == 0:
+                cnt += 1
+                break
+            else:
+                cnt += 1
+                M -= 1
         else:
-            result.append('-')
-    return '\n'.join(result)
-
-print(numeric())
+            a = imp.popleft()
+            imp.append(a)
+            if M == 0:
+                M += len(imp) - 1
+            else:
+                M -= 1
+    print(cnt)
