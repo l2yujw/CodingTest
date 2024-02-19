7c2c644dcd99bf7e4aee6f149eba1aa6e2a2734a
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2024-02-19 16:18:01 +0900

    [Silver V] Title: 나이순 정렬, Time: 112 ms, Memory: 39996 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,12 +1,13 @@
-n = int(input())
-member_lst = []
+import sys
 
-for i in range(n):
-    age, name = map(str, input().split())
-    age = int(age)
-    member_lst.append((age, name))
+T = int(input())
 
-member_lst.sort(key = lambda x : x[0])	## (age, name)에서 age만 비교
+N = []
 
-for i in member_lst:
-    print(i[0], i[1])
+input = sys.stdin.readline
+for i in range(0, T) :
+    N.append(input())
+
+N.sort(key=lambda x: int(x.split()[0]))
+
+print(''.join(N))
