4c23e2bd952ebdebee5b7b988e0bf8506d86d12f
ryu <jeungwon28@gmail.com>
2024-03-18 22:42:23 +0900

    ADD: DFS/BFS

---
+++ TextBook/ch5-8.py
@@ -0,0 +1,23 @@
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end = ' ')
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+#각 노드 번호에 연결 되어 있는 노드의 숫자
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+visited = [False] * 9
+
+dfs(graph, 1, visited)
