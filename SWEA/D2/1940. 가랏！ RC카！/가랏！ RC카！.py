a86d983d5df7222d2970e2aaa9db7a831953e3b4
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2024-05-12 21:53:01 +0900

    [D2] Title: 가랏! RC카!, Time: 103 ms, Memory: 43,708 KB -BaekjoonHub

---
+++ SWEA/D2/1940. 가랏！ RC카！/가랏！ RC카！.py
@@ -0,0 +1,21 @@
+T = int(input())
+for test_case in range(1, T + 1):
+    n = int(input())
+    arr = []
+    v, d = 0, 0
+    for i in range(n):
+        arr.append(list(map(int, input().split())))
+
+    for x in arr:
+        if x[0] == 0:
+            d += v
+        elif x[0] == 1:
+            v += x[1]
+            d += v
+        elif x[0] == 2:
+            v -= x[1]
+            if v < 0:
+                v = 0
+            d += v
+
+    print("#%d %d" % (test_case, d))
