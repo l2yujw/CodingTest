dca8c401bb8fc05805624891b4a14a47e5ccf404
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2024-05-13 11:47:42 +0900

    [D2] Title: 스도쿠 검증, Time: 103 ms, Memory: 43,968 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -1,28 +1,21 @@
 T = int(input())
-for test_case in range(1, T + 1):
-    arr = [list(map(int, input().split())) for _ in range(9)]
+for tc in range(1, T + 1):
+    arr = [[0 for _ in range(9)] for _ in range(9)]
+    for i in range(9):
+        arr[i] = list(map(int, input().split()))
+    rows = [set() for _ in range(9)]
+    cols = [set() for _ in range(9)]
+    boxes = [set() for _ in range(9)]
 
-    result = 1
-    row, col, box = [], [], []
-    pat = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    for y in range(9):
-        row.append(arr[y])
-        tmp = []
-        for x in range(9):
-            tmp.append(arr[x][y])
-        col.append(tmp)
+    flag = 1
+    for i in range(9):
+        for j in range(9):
+            num = arr[i][j]
+            idx = (i // 3) * 3 + (j // 3)
 
-    for x in range(0, 9, 3):
-        tmp = []
-        for y in range(9):
-            tmp += arr[y][x: x + 3]
-            if y % 3 == 2:
-                box.append(tmp)
-                tmp = []
-
-    for y in range(9):
-        if sorted(row[y]) != pat or sorted(col[y]) != pat or sorted(box[y]) != pat:
-            result = 0
-            break
-
-    print("#%d %d" % (test_case, result))
+            if num in rows[i] or num in cols[j] or num in boxes[idx]:
+                flag = 0
+            rows[i].add(num)
+            cols[j].add(num)
+            boxes[idx].add(num)
+    print(f'#{tc} {flag}')
