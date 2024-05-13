1df3a953636c91d0ed5340163988f2e53eeb5b59
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2024-05-13 12:53:11 +0900

    [D2] Title: 시각 덧셈, Time: 95 ms, Memory: 44,072 KB -BaekjoonHub

--- SWEA/D2/1976. 시각 덧셈/시각 덧셈.py
+++ SWEA/D2/1976. 시각 덧셈/시각 덧셈.py
@@ -2,11 +2,8 @@ T = int(input())
 for test_case in range(1, T + 1):
     h1, m1, h2, m2 = map(int, input().split())
 
-    hour = h1 + h2
-    minute = m1 + m2
-    if minute >= 60:
-        hour += (minute // 60)
-        minute %= 60
+    minute = (m1 + m2) % 60
+    hour = (h1 + h2) + ((m1 + m2) // 60)
 
     hour %= 12
     if hour == 0:
