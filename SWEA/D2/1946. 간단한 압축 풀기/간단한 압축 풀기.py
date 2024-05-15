71cceefe7ef10926e57277cc9c70b6c2e7964992
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2024-05-15 18:33:30 +0900

    [D2] Title: 간단한 압축 풀기, Time: 103 ms, Memory: 43,692 KB -BaekjoonHub

--- SWEA/D2/1946. 간단한 압축 풀기/간단한 압축 풀기.py
+++ SWEA/D2/1946. 간단한 압축 풀기/간단한 압축 풀기.py
@@ -1,20 +1,11 @@
 T = int(input())
 for test_case in range(1, T + 1):
     N = int(input())
-    arr = []
+    arr = ""
     for i in range(N):
         a, n = map(str, input().split())
-        arr.append([a, n])
+        arr += a * int(n)
 
-    len = 0
     print(f"#{test_case}")
-    for i in arr:
-        for _ in range(int(i[1])):
-            if len == 10:
-                len = 0
-                print()
-                print(i[0], end="")
-            else:
-                print(i[0], end="")
-            len += 1
-    print()
+    for k in range(len(arr) // 10 + 1):
+        print(arr[10 * k: 10 * k + 10])
