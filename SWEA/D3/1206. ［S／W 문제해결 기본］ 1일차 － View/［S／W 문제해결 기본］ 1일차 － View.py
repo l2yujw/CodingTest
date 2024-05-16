02acc1af01ab55c24cd83e0320d4cc0754ceb159
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2024-05-16 11:27:15 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 143 ms, Memory: 47,976 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
@@ -3,11 +3,10 @@ for test_case in range(1, 11):
     arr = list(map(int, input().split()))
     result = 0
     for i in range(2, n - 2):
-        if arr[i - 2] >= arr[i] or arr[i - 1] >= arr[i] or arr[i + 1] >= arr[i] or arr[i + 2] >= arr[i]:
+        left = max(arr[i-2:i])
+        right = max(arr[i+1:i+3])
+        if left >= arr[i] or right >= arr[i]:
             continue
         else:
-            left = max(arr[i-2:i])
-            right = max(arr[i+1:i+3])
-
             result += arr[i] - max(left, right)
     print(f"#{test_case} {result}")
