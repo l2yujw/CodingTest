ec3084b81a5a3e398ac798ba527791d557a80ef4
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2024-05-18 21:21:57 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문1, Time: 103 ms, Memory: 43,976 KB -BaekjoonHub

---
+++ SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.py
@@ -0,0 +1,29 @@
+for test_case in range(1, 11):
+    n = int(input())
+    origin = list(map(int, input().split()))
+    m = int(input())
+    inst = list(input().split())
+
+    type = ''
+    pos = -1
+    cnt = -1
+    for i in range(len(inst)):
+        if inst[i] == "I":
+            type = inst[i]
+            pos = -1
+            cnt = -1
+        else:
+            if type == "I":
+                if pos == -1:
+                    pos = int(inst[i])
+                    continue
+                else:
+                    if cnt == -1:
+                        cnt = int(inst[i])
+                        continue
+
+                    origin.insert(pos, inst[i])
+                    pos += 1
+
+    print(f"#{test_case}", end=' ')
+    print(*origin[:10])
