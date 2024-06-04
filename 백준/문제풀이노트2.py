4572ce934eaeccde35764d816fd304f82194d96f
ryu <jeungwon28@gmail.com>
2024-06-04 14:39:05 +0900

    RGB거리 dfs풀이

--- 백준/문제풀이노트2.py
+++ 백준/문제풀이노트2.py
@@ -1,19 +1,12 @@
-N, M = map(int, input().split())
-numbers = [int(x) for x in input().split()]
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
 
-numbers.sort()
+dp = [[0]*3 for _ in range(n)]
+dp[0] = arr[0]
 
-def backtracking(depth):
-    if depth == M:
-        print(' '.join(map(str,box)))
-        return
+for i in range(1,n):
+    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + arr[i][0]
+    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + arr[i][1]
+    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + arr[i][2]
 
-    for i in range(N):
-        if numbers[i] in box:
-            continue
-        box.append(numbers[i])
-        backtracking(depth + 1)
-        box.pop()
-
-box = []
-backtracking(0)
+print(min(dp[n-1]))
