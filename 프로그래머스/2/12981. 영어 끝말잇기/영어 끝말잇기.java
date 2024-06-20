99a130a936e9b39e6b93be131e7fe5f2ab964284
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2024-06-20 18:52:13 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = new int[2];
+
+        Set<String> record = new HashSet<>();
+
+        record.add(words[0]);
+        char last = words[0].charAt(words[0].length() - 1);
+        for (int i = 1; i < words.length; i++) {
+            if (record.contains(words[i]) || last != words[i].charAt(0)) {
+                answer[0] = i % n + 1;
+                answer[1] = i / n + 1;
+                break;
+            } else {
+                record.add(words[i]);
+            }
+            last = words[i].charAt(words[i].length() - 1);
+        }
+
+        return answer;
+    }
+}
