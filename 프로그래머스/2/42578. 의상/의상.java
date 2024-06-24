ad73bc6f63fbe8cd1958d0211cda0e01e754902a
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2024-06-24 16:42:20 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<>();
+        for (String[] clothe : clothes) {
+            map.put(clothe[1], map.getOrDefault(clothe[1], 0) + 1);
+        }
+
+        for (Integer value : map.values()) {
+            answer *= value + 1;
+        }
+        return answer - 1;
+    }
+}
