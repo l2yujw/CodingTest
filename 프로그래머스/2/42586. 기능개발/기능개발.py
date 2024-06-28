e8afaa1848620c38ec4749effacacfb7a72af7ea
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2024-06-28 21:44:35 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+
+def solution(progress, speeds):
+    answer = []
+    days = deque()
+
+    for i in range(len(progress)):
+        x = (100 - progress[i]) / speeds[i]
+
+        if x == int(x):
+            days.append(int(x))
+        else:
+            days.append(int(x) + 1)
+
+    while days:
+        temp = days.popleft()
+        count = 1
+
+        while days and temp >= days[0]:
+            days.popleft()
+            count += 1
+        answer.append(count)
+
+    return answer
