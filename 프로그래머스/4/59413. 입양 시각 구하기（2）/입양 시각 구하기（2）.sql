659242a9113828c89bc14c0f2be06d71ae4b4ea1
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2024-07-30 00:27:14 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -0,0 +1,8 @@
+SET @hour := -1;
+
+SELECT @hour := @hour + 1 hour,
+    (SELECT COUNT(HOUR(datetime))
+    FROM animal_outs
+    WHERE HOUR(datetime) = @hour) count
+FROM animal_outs
+WHERE @hour + 1 < 24
