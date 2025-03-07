9c3da2d2fcfeb279e5535eabf488f68eb53d9f7c
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2025-03-07 16:25:34 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,12 @@
+-- 코드를 입력하세요
+-- animal_ins 동물 보호소에 들어온 동물들의 정보
+-- animal_outs 입양 보낸 동물들의 정보
+-- ins (입양 못간 애, 가장 오래 3마리,) => 이름, 보호시작일(시작일 순)
+SELECT  ai.name, ai.datetime
+FROM    animal_ins ai
+WHERE   ai.animal_id NOT IN (
+                        SELECT  ao.animal_id
+                        FROM    animal_outs ao
+                    )
+ORDER BY    ai.datetime
+LIMIT   3
