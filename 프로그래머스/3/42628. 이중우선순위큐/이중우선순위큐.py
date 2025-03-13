b52b909dc0599d88becb5cda77e9d09c9766fc6d
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2025-03-13 11:55:11 +0900

    [level 3] Title: 이중우선순위큐, Time: 81.57 ms, Memory: 18.9 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,23 +1,40 @@
 import heapq
 
 def solution(operations):
-    heap = []
-    for oper in operations:
-        arr = list(oper.split())
-        command = arr[0]
-        num = int(arr[1])
-        
-        if command == 'I': 
-            heapq.heappush(heap, num)
-        elif command == 'D':
-            if heap:
-                if num == 1:
-                    heap.sort()
-                    heap.pop()
-                else:
-                    heapq.heappop(heap)
-    
-    if heap:
-        return [heapq.nlargest(1, heap)[0], heapq.nsmallest(1, heap)[0]]
-    else: 
+    min_heap = []
+    max_heap = []
+    entry = {}
+
+    for op in operations:
+        cmd, num = op.split()
+        num = int(num)
+
+        if cmd == "I":
+            heapq.heappush(min_heap, num)
+            heapq.heappush(max_heap, -num)
+            entry[num] = entry.get(num, 0) + 1
+
+        elif cmd == "D":
+            if num == 1:
+                while max_heap and entry.get(-max_heap[0], 0) == 0:
+                    heapq.heappop(max_heap)
+                if max_heap:
+                    max_value = -heapq.heappop(max_heap)
+                    entry[max_value] -= 1
+
+            elif num == -1:
+                while min_heap and entry.get(min_heap[0], 0) == 0:
+                    heapq.heappop(min_heap)
+                if min_heap:
+                    min_value = heapq.heappop(min_heap)
+                    entry[min_value] -= 1
+
+    while min_heap and entry.get(min_heap[0], 0) == 0:
+        heapq.heappop(min_heap)
+    while max_heap and entry.get(-max_heap[0], 0) == 0:
+        heapq.heappop(max_heap)
+
+    if min_heap and max_heap:
+        return [-max_heap[0], min_heap[0]]
+    else:
         return [0, 0]
