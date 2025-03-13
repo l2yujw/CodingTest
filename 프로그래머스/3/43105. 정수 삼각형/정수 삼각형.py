e2066efadf2909b6029b96c8affee80bad5fcd94
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2025-03-13 10:21:11 +0900

    [level 3] Title: 정수 삼각형, Time: 35.46 ms, Memory: 14 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,13 +1,10 @@
-# 삼각형 꼭대기 -> 바닥 경로, 거쳐간 숫자의 합이 가장 큰
-# 대각하단 좌우 가능
-# list [7] [10 15] [max(18), max(11, 16)]
 def solution(triangle):
-    for i in range(1, len(triangle)):
-        for k in range(i + 1):
-            if k == 0:
-                triangle[i][k] += triangle[i - 1][k]
-            elif k == i:
-                triangle[i][k] += triangle[i - 1][k - 1]
-            else:
-                triangle[i][k] += max(triangle[i - 1][k - 1], triangle[i - 1][k])
-    return max(triangle[len(triangle) - 1])
+
+    floor = len(triangle) - 1 
+
+    while floor > 0:
+        for i in range(floor):
+            triangle[floor-1][i] += max(triangle[floor][i], triangle[floor][i+1])
+        floor -= 1
+
+    return triangle[0][0]
