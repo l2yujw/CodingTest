9265948ceac470e990741983d0d66fd71cda231f
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2025-03-18 10:06:35 +0900

    [level 3] Title: 최고의 집합, Time: 0.87 ms, Memory: 9.7 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.py
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.py
@@ -2,13 +2,14 @@
 # {a, b} a+b=S, a*b := max => |a - b| := min
 # {a, b, c} a+b+c=S, a*b*c := max => 차가 최소
 def solution(n, s):
-    
     if s < n:
         return [-1]
     
     base = s // n
     answer = list(base for _ in range(n))
+    
     rest = s % n
     for i in range(0, rest):
         answer[i] += 1
+        
     return sorted(answer)
