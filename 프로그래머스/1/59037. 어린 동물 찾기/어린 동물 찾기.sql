13e9eeb594b498644d28dc1fadebf816426f4d1d
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2025-07-28 11:56:34 +0900

    [level 1] Title: 어린 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59037. 어린 동물 찾기/어린 동물 찾기.sql
+++ 프로그래머스/1/59037. 어린 동물 찾기/어린 동물 찾기.sql
@@ -1,2 +1,5 @@
 -- 코드를 입력하세요
-SELECT animal_id, name from animal_ins where not intake_condition like 'Aged' order by animal_id
+SELECT animal_id, name 
+FROM animal_ins 
+WHERE intake_condition NOT LIKE 'Aged' 
+ORDER BY animal_id
