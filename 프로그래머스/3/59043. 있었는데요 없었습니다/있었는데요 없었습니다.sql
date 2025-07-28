d571828979008da1cbbe580cf900066136d320f4
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2025-07-28 12:43:28 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,6 +1,7 @@
 -- 코드를 입력하세요
 -- 보호시작일 < 입양일 => 동물의 아이디, 이름 (보호 시작일 빠른순)
 SELECT  ai.animal_id, ai.name
-FROM    animal_ins ai JOIN animal_outs ao ON ai.animal_id = ao.animal_id
+FROM    animal_ins ai 
+JOIN animal_outs ao ON ai.animal_id = ao.animal_id
 WHERE   ai.datetime - ao.datetime > 0
 ORDER BY    ai.datetime
