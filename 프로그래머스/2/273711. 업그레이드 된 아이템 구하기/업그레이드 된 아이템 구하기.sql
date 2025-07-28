aa025a80adecb1111495bdd5ed695f2287bf371c
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2025-07-28 14:38:06 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -1,10 +1,11 @@
 -- 코드를 작성해주세요
-select  i.item_id, i.item_name, i.rarity
-from    item_tree t, item_info i
-where   t.item_id = i.item_id and
-        parent_item_id in (
-                select  item_id
-                from    item_info
-                where   rarity = "rare"
-        )
-order by    i.item_id desc
+WITH RareItems AS (
+    SELECT item_id
+    FROM item_info
+    WHERE rarity = 'rare'
+)
+SELECT i.item_id, i.item_name, i.rarity
+FROM item_tree t
+JOIN item_info i ON t.item_id = i.item_id
+JOIN RareItems r ON t.parent_item_id = r.item_id
+ORDER BY i.item_id DESC;
