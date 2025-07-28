87b3e6d39239ca5b6e7c70d35d51d14dae413d1b
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2025-07-28 12:44:00 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,8 +1,5 @@
--- 코드를 입력하세요
--- 입양O, 보호기간 가장긴 둘 => 아이디 이름 (보호기간 긴순)
 SELECT      ai.animal_id, ai.name
 FROM        animal_ins ai 
-            JOIN animal_outs ao
-            ON ai.animal_id = ao.animal_id
-ORDER BY    ao.datetime - ai.datetime desc
+JOIN        animal_outs ao ON ai.animal_id = ao.animal_id
+ORDER BY    DATEDIFF(ao.datetime, ai.datetime) DESC
 LIMIT       2
