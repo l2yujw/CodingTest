8ee231c61afe156513863ab05b2893559ae05c2e
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2025-07-29 15:51:55 +0900

    [level 2] Title: 올바른 괄호, Time: 6.42 ms, Memory: 9.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+def solution(s):
+    answer = 0
+    
+    for i in s:
+        if i == '(':
+            answer += 1
+        else:
+            answer -= 1
+        
+        if answer == -1:
+            return False
+
+    if answer == 0:
+        return True
+    else: 
+        return False
