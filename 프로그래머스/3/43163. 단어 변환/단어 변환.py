df38262e6ba809eda7ecf1b47ccc0dcf58c994bc
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2025-08-08 13:40:41 +0900

    [level 3] Title: 단어 변환, Time: 1.81 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,30 +1,42 @@
-# words[[begin, target]]
-# 한 번에 한개의 알파벳만 변경 가능
-# words에 있는 단어로만 변환 가능
+# 한 번에 한 개의 알파벳 변경
+# words에 있어야 함
+
 
 def solution(begin, target, words):
     if target not in words:
         return 0
     
-    visited = [False] * len(words)
-    return dfs(begin, target, words, visited, 0)
-
-def dfs(current, target, words, visited, count):
-    if current == target:
-        return count
+    answer = [int(1e9)]
     
-    min_count = float('inf')
+    words = [begin] + words
+    visited = [False] * len(words)
+    graph = [[] for _ in range(len(words))]
     
     for i in range(len(words)):
-        if not visited[i] and is_one_matched(current, words[i]):
-            visited[i] = True
-            result = dfs(words[i], target, words, visited, count + 1)
-            if result:
-                min_count = min(result, min_count)
-            visited[i] = False
-            
-    return min_count if min_count != float('inf') else 0
-            
-def is_one_matched(word1, word2):
-    diff_count = sum([word1[i] != word2[i] for i in range(len(word1))])
-    return diff_count == 1
+        for j in range(i + 1, len(words)):
+            if is_changable(words[i], words[j]):
+                graph[i].append(j)
+                graph[j].append(i)
+
+    dfs(graph, visited, words, target, 0, 0, answer)
+    
+    return answer[0] if answer[0] != int(1e9) else 0
+
+
+def is_changable(word, target):
+    count = sum(word[i] != target[i] for i in range(len(word)))
+    return count == 1
+    
+    
+def dfs(graph, visited, words, target, v, count, answer):
+    if words[v] == target:
+        answer[0] = min(answer[0], count)
+        return
+    
+    visited[v] = True
+    
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, visited, words, target, i, count + 1, answer)
+    visited[v] = False
+
