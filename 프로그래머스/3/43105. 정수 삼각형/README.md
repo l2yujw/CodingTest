f01c8a042b4e3f44eeba970e0a2ac6d8d5f74742
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2025-08-08 11:22:55 +0900

    [level 3] Title: 정수 삼각형, Time: 43.63 ms, Memory: 13.8 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/README.md
+++ 프로그래머스/3/43105. 정수 삼각형/README.md
@@ -1,10 +1,10 @@
 # [level 3] 정수 삼각형 - 43105 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105) 
 
 ### 성능 요약
 
-메모리: 14 MB, 시간: 35.46 ms
+메모리: 13.8 MB, 시간: 43.63 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 13일 10:21:09
+2025년 08월 08일 11:22:52
 
 ### 문제 설명
 
--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,10 +1,30 @@
 def solution(triangle):
 
+    answer = top_down(triangle)
+
+    return answer
+
+def bottom_up(triangle):
     floor = len(triangle) - 1 
 
     while floor > 0:
         for i in range(floor):
             triangle[floor-1][i] += max(triangle[floor][i], triangle[floor][i+1])
         floor -= 1
-
+        
     return triangle[0][0]
+
+def top_down(triangle):
+    floor = 1
+    
+    while floor < len(triangle):
+        for i in range(floor + 1):
+            if i == 0:
+                triangle[floor][i] += triangle[floor-1][0]
+            elif i == floor:
+                triangle[floor][i] += triangle[floor-1][i-1]
+            else:
+                triangle[floor][i] += max(triangle[floor-1][i-1], triangle[floor-1][i])
+        floor += 1
+        
+    return max(triangle[len(triangle)-1])
