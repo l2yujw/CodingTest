c8edeb2f1da8afc0ff60e8bbcbf2fe72a3d5b100
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2025-08-11 10:53:09 +0900

    [level 3] Title: 입국심사, Time: 607.49 ms, Memory: 13.6 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,11 +1,17 @@
-# n명 입국심사
-# 한 심사대에 한명, 가장 앞에 서 있는 사람 비어있는 심사대
-# 모든 사람이 심사를 받는데 걸리는 시간 최소
+# n명 입국 심사
+# 한 심사대 동시에 한명
+# 가장 앞 사람 비어 있는 곳으로 가서 심사
+# 더 빨리 끝나는 심사대가 있으면 그곳으로 가서 심사
+
+# n: 사람, times: 각 심사관 한명 심사 걸리는 시간
+# return 모든 사람 심사 소요 시간 최솟값
 
 def solution(n, times):
+    
     answer = 0
     
     left, right = 1, max(times) * n
+    
     while left <= right:
         mid = (left + right) // 2
         p = sum(mid // time for time in times)
@@ -15,5 +21,5 @@ def solution(n, times):
             right = mid - 1
         else:
             left = mid + 1
-        
+    
     return answer
