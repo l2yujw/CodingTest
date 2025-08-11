c500491cc801c285a9b7a4a07056f40de072c01e
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2025-08-11 11:41:03 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 19.27 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -3,11 +3,12 @@
 # (0,0) 시작, 도착(N-1,N-1)
 # 상하좌우 벽 불가
 # 직선 100원, 코너 500원
-# 최소비용 구해라
+# 최소비용 구해라 = bfs why? 내가 다음 간 길이 무조건 최소
+
 from collections import deque
+
 def solution(board):
-    answer = min(bfs(0, 0, 0, 1, board), bfs(0, 0, 0, 3, board))
-    return answer
+    return min(bfs(0, 0, 0, 1, board), bfs(0, 0, 0, 3, board))
 
 
 def bfs(y, x, cost, dir, b):
@@ -15,23 +16,21 @@ def bfs(y, x, cost, dir, b):
     dx = [0, 0, -1, 1]
     l = len(b)
     
-    visited = [[ [float('inf')] * 4 for _ in range(l)] for _ in range(l)]
-    for d in range(4):
-        visited[y][x][d] = 0
+    INF = int(1e9)
+    
+    visited = [[INF] * l for _ in range(l)]
     
     q = deque()
     q.append((y, x, cost, dir))
+    
     while q:
         y, x, cost, dir = q.popleft()
         for i in range(4):
             ny, nx = y + dy[i], x + dx[i]
             if 0 <= ny < l and 0 <= nx < l and b[ny][nx] == 0:
-                if i == dir:
-                    ncost = cost + 100
-                else:
-                    ncost = cost + 600
-                
-                if visited[ny][nx][i] > ncost:
-                    visited[ny][nx][i] = ncost
+                ncost = cost + (100 if i == dir else 600)
+                if ncost < visited[ny][nx]:
+                    visited[ny][nx] = ncost
                     q.append((ny, nx, ncost, i))
-    return min(visited[-1][-1])
+                    
+    return visited[-1][-1]
