39c40337b77c2efc3bed6abcd1cb8289cfc0fe97
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2025-08-11 13:37:44 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 134.73 ms, Memory: 19.9 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/README.md
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/README.md
@@ -1,10 +1,10 @@
 # [level 3] 다단계 칫솔 판매 - 77486 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77486?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77486) 
 
 ### 성능 요약
 
-메모리: 20.4 MB, 시간: 283.93 ms
+메모리: 19.9 MB, 시간: 134.73 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 08일 10:55:17
+2025년 08월 11일 13:37:40
 
 ### 문제 설명
 
--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.py
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.py
@@ -1,38 +1,30 @@
-# 판매 수익 얻음
-# 자신이 참여시킨 추천인에게서 발생하는 10%까지 자신의 이익
-# 마찬가지로 자신을 추천해준 사람에게 발생하는 이익의 10%감
-# 10% 계산 금액이 1원미만이면 자신이 가짐
+# 추천인으로 부터 받은 10%까지 포함해서 위로 10% 제공
 
-# enroll: 판매원 이름, referral: 추천인 이름 (enroll i번째에 있는 판매원의)
-# seller: 판매한 사람, amount: 판매량(seller i번째에 있는 판매원의)
-# 칫솔 한 개 이익 100원 고정
+# enroll: 판매원 이름, referral: 자기를 참여시킨 판매원
+# seller, amount: 사람, 판매량
 
-# enroll dict 이름, i for문
-# graph 0~len[enroll]
-# referral graph[dict[referral[i]]].append(i)
-# dfs 타고 들어가다가 dict[seller[i]] == 현재 판매원 i
-# 지금까지 방문한 visited의 i번째 결과에 10%씩해서 더하기
-# 조건 방문해제 필요
-# dfs(x) 환경
-# for i in graph[x] 방문처리 if seller 조건: 결과합,
-# dfs(i) 방문처리 해제
+# "-" center직할
+# amount * 100 보장
+
 def solution(enroll, referral, seller, amount):
-    name_to_idx = {name: i for i, name in enumerate(enroll)}
+    enroll_idx = {name: i for i, name in enumerate(enroll)}
     result = [0] * len(enroll)
-
-    ref_map = {}
-    for i, name in enumerate(enroll):
-        ref_map[name] = referral[i]
-
-    def dfs(person, money):
-        if person == "-" or money < 1:
-            return
-        idx = name_to_idx[person]
-        give = money // 10
-        result[idx] += money - give
-        dfs(ref_map[person], give)
-
+    
+    parent = [-1] * len(enroll)
+    for i in range(len(referral)):
+        if referral[i] != '-':
+            parent[i] = enroll_idx[referral[i]]
+        
     for i in range(len(seller)):
-        dfs(seller[i], amount[i] * 100)
-
+        dfs(parent, enroll_idx[seller[i]], amount[i] * 100, result)
+    
     return result
+        
+        
+def dfs(parent, idx, money, result):
+    if idx == -1 or money < 1:
+        return
+    give = money // 10
+    get = money - give
+    result[idx] += get
+    dfs(parent, parent[idx], give, result)
