23645bbd4ed685c26be5e4d1f425cb98b55118ca
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2025-11-24 23:55:24 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -4,4 +4,4 @@ SELECT  ai.animal_id, ai.name
 FROM    animal_ins ai 
 JOIN animal_outs ao ON ai.animal_id = ao.animal_id
 WHERE   ai.datetime - ao.datetime > 0
-ORDER BY    ai.datetime
+ORDER BY    ai.datetime;
