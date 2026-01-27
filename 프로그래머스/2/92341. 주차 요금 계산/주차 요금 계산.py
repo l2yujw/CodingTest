74eecbc2cf4d947d0b127e31336a79377195940a
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2026-01-27 11:22:45 +0900

    [level 2] Title: 주차 요금 계산, Time: 1.60 ms, Memory: 9.46 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -1,39 +1,40 @@
 import math
+
 def solution(fees, records):
-    answer = []
-    cars = {}
-    answer = {}
-    for i in records:
-        split = i.split()
-        if split[2] == 'IN':
-            cars[split[1]] = split[0]
+    parking_cars = {}   # 현재 주차중인 차량: car -> 입차시간
+    parking_time = {}   # 누적 주차 시간: car -> minutes
+    parking_fee = {}    # 최종 요금: car -> fee
+
+    # 입출차 기록 처리
+    for record in records:
+        time, car, action = record.split()
+
+        if action == "IN":
+            parking_cars[car] = time
+        else:  # OUT
+            in_h, in_m = map(int, parking_cars[car].split(":"))
+            out_h, out_m = map(int, time.split(":"))
+            minutes = (out_h - in_h) * 60 + (out_m - in_m)
+
+            parking_time[car] = parking_time.get(car, 0) + minutes
+            parking_cars.pop(car)
+
+    # 출차 안 한 차량 (23:59 처리)
+    for car, time in parking_cars.items():
+        in_h, in_m = map(int, time.split(":"))
+        minutes = (23 - in_h) * 60 + (59 - in_m)
+        parking_time[car] = parking_time.get(car, 0) + minutes
+
+    # 요금 계산
+    base_time, base_fee, unit_time, unit_fee = fees
+
+    for car, total_time in parking_time.items():
+        if total_time <= base_time:
+            parking_fee[car] = base_fee
         else:
-            inTime = cars.get(split[1]).split(":")
-            outTime = split[0].split(":")
-            minute = (int(outTime[0]) - int(inTime[0])) * 60 + (int(outTime[1]) - int(inTime[1]))
-            if split[1] in answer.keys():
-                answer[split[1]] += minute
-            else:
-                answer[split[1]] = minute
-            pop = cars.pop(split[1])
+            parking_fee[car] = base_fee + math.ceil(
+                (total_time - base_time) / unit_time
+            ) * unit_fee
 
-    for i in cars:
-        inTime = cars.get(i).split(":")
-        minute = (23 - int(inTime[0])) * 60 + (59 - int(inTime[1]))
-        if i in answer.keys():
-            answer[i] += minute
-        else:
-            answer[i] = minute
-
-    for i in answer:
-        if answer[i] <= fees[0]:
-            answer[i] = fees[1]
-        else:
-            answer[i] = fees[1] + math.ceil((answer[i] - fees[0]) / fees[2]) * fees[3]
-
-    d = sorted(answer)
-    ans = []
-    for i in d:
-        ans.append(answer[i])
-
-    return ans
+    # 차량 번호 순 정렬 후 결과 반환
+    return [parking_fee[car] for car in sorted(parking_fee)]
