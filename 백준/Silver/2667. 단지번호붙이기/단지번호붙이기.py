217d1be6f2d85f2e171c988a1cc05894ed857ae7
l2yuPa <49338509+l2yujw@users.noreply.github.com>
2026-02-03 11:47:11 +0900

    [Silver I] Title: 단지번호붙이기, Time: 48 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,44 +1,40 @@
-from sys import stdin
+# 총단지수, 각 단지내 집 수 오름차순
 from collections import deque
-input = stdin.readline
 
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
+def bfs(y, x):
+    n = len(matrix)
+    q = deque([(y, x)])
+    matrix[y][x] = 0
+    cnt = 1
 
-def bfs(graph, y, x):
-    n = len(graph)
-    queue = deque()
-    queue.append((y, x))
-    graph[y][x] = 0
-    count = 1
-
-    while queue:
-        y, x = queue.popleft()
+    while q:
+        y, x = q.popleft()
         for i in range(4):
-            ny = y + dy[i]
-            nx = x + dx[i]
-            if nx < 0 or nx >= n or ny < 0 or ny >= n:
-                continue
-            if graph[ny][nx] == 1:
-                graph[ny][nx] = 0
-                queue.append((ny, nx))
-                count += 1
-    return count
+            ny, nx = y + dy[i], x + dx[i]
+            if 0 <= nx < n and 0 <= ny < n and matrix[ny][nx] == 1:
+                matrix[ny][nx] = 0
+                q.append((ny, nx))
+                cnt += 1
+
+    return cnt
 
 
-n = int(input().rstrip())
-graph = []
+N = int(input())
 
-for i in range(n):
-    graph.append(list(map(int, input().rstrip())))
+matrix = []
+dy = [-1, 1, 0, 0]
+dx = [0, 0, -1, 1]
+
+for i in range(N):
+    matrix.append(list(map(int, input().rstrip())))
+
 cnt = []
 
-for i in range(n):
-    for j in range(n):
-        if graph[i][j] == 1:
-            cnt.append(bfs(graph, i, j))
+for y in range(N):
+    for x in range(N):
+        if matrix[y][x] == 1:
+            cnt.append(bfs(y, x))
 
 cnt.sort()
 print(len(cnt))
-for i in range(len(cnt)):
-    print(cnt[i])
+print(*cnt)
